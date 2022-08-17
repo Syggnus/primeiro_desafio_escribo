d7b06ef8cf7735289224a6c56ecf3936afbf0ad4
Syggnus <chuan.ferreira@outlook.com>
2022-08-17 17:30:41 -0300

    Create main.dart
    Criação da main do Programa

---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+import 'package:desafio_escribo_1/somar_divisores.dart';
+
+///Função main para iniciar o programa.
+void main() {
+  print("Seja Bem-vindo(a) ao Primeiro Desafio da Escribo.");
+
+  ///Chamando a Função somarNumerosDivisiveis().
+  somarNumerosDivisiveis();
+}
