984523c8e4984972bf22a6a41a5d9dee81e9e357
Syggnus <chuan.ferreira@outlook.com>
2022-08-17 17:38:04 -0300

    Update main.dart
    Alteração e correção dos nomes do package

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
-import 'package:desafio_escribo_1/somar_divisores.dart';
+import 'package:desafio_escribo_1/somar_dividendos.dart';
 
 ///Função main para iniciar o programa.
 void main() {
   print("Seja Bem-vindo(a) ao Primeiro Desafio da Escribo.");
 
   ///Chamando a Função somarNumerosDivisiveis().
-  somarNumerosDivisiveis();
+  somarDividendos();
 }
